928c0024065330dd0f5d227fc263bcf337ec6614
Almir Jr <99154364+AlmirJrDev@users.noreply.github.com>
2023-11-29 11:58:11 -0300

    table

--- src/components/Budget/index.tsx
+++ src/components/Budget/index.tsx
@@ -1,12 +1,49 @@
+import { BudgetContainer, FormBudget, InputName } from "./style";
+
 export function BudgetSheet() {
+  const InputCPF = document.getElementById("#CPF")
+
+  InputCPF?.addEventListener('keypress', () => {
+    const inputlength = InputCPF.value.length
+
+    console.log(inputlength)
+  })
   return(
-    <div> <h1>Ficha de orçamento</h1>
-      <div>
-        <form>g
-          <label for="client">Client: <input type="text" /></label>
+    <BudgetContainer> <h1>Ficha de orçamento</h1>
+      <FormBudget>
+        <form>
+        <table>
+<tbody>
+<tr>
+<InputName><label >Client: <input type="text" /></label></InputName>
+<td><label>Tel: <input type="number" /></label> </td>
+<td><label>Data: <input type="date" /></label> </td>
+</tr>
+</tbody>
+</table>
+<table>
+<tbody>
+<tr>
+<td><label >Endereço: <input type="text" /></label></td>
+<td><label>CPF: <input id="#CPF" type="text" maxLength={14}/></label> </td>
+</tr>
+</tbody>
+</table>
+<table>
+<tbody>
+  <tr>
+<td><label>Serviços: <input type="text" /></label></td>
+<td><label>Horas: <input type="text" /></label> </td>
+<td><label>Valor: <input type="text" /></label> </td>
+</tr>
+</tbody>
+</table>
+
+
+           
         </form>
-      </div>
+      </FormBudget>
       
-    </div>
+    </BudgetContainer>
   )
 }
---
+++ src/components/Budget/style.ts
@@ -0,0 +1,54 @@
+import { styled } from "styled-components";
+
+export const BudgetContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+export const FormBudget = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+background: #d9d9d7;
+
+ width: 50vw;
+  input{
+    
+    border: none;
+    width: 100px;
+
+    background: #d9d9d7;
+    
+    &:focus{
+      outline-width: 0;
+    }
+
+    &::-webkit-outer-spin-button,
+&::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+  }
+  table{
+    width: 50vw;
+  }
+  td{
+    height: 25px;
+    border: 1px solid #000;
+    label{
+    text-align: center;
+  }
+  }
+
+
+ 
+`
+export const InputName = styled.td`
+  width:300px;
+  input{
+    width: 75%;
+    
+  }
+  
+`
